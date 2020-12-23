8b838556af8ade93351b2360ccffe2ace871d9ea
Jeanette Perez <jeanette.marie.perez@gmail.com>
2020-12-23 06:32:25 -0600

    adding readme, function drills, and the html for this repo

---
+++ js/drills.js
@@ -0,0 +1,27 @@
+function isOdd(number) {
+    return number % 2 !== 0;
+}
+
+function isEven(number) {
+    return number % 2 === 0;
+}
+
+function identity(input) {
+    return input;
+}
+
+function isFive(number) {
+    return number === 5;
+}
+
+function addFive(input) {
+    return input + 5;
+}
+
+function isMultipleOfFive(input) {
+    return input % 5 === 0;
+}
+
+function isThree(input) {
+    return input === 3;
+}
